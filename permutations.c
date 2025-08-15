3d5b61acc921ebe886553e3c821d5fbffbb1baf0
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-08-15 18:01:09 +0000

    solved permutation ques

---
+++ permutations.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <time.h>
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <limits.h>
+#include <ctype.h>
+
+void permutations(int *arr, int n, int *b, int k, int cur){
+    if(cur == k){
+        for(int i = 0; i < k; i++){
+            printf("%d ", b[i]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < n; i++){
+        swap(&arr[0], &arr[i]);
+        b[k] = arr[0];
+        permutations(arr + 1, n - 1, b, k, cur + 1);
+        swap(&arr[0], &arr[i]);
+    }
+} 
+
+int main(){
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int arr[n], b[k];
+    for(int i = 0; i < n; i++)
+        arr[i] = i;
+    permutations(arr, n, b, k, 0);
+    return 0;
+}
