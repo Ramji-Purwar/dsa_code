13f1aa938f1cbdb1fa83dbd90e83cdeb9e2b3cc3
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-08-20 19:03:13 +0000

    tried closed knightTour

--- knightTour.c
+++ knightTour.c
@@ -1,70 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <stdint.h>
-#include <time.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <math.h>
-#include <float.h>
-#include <ctype.h>
 
-size_t movements[8][2] = {
-    {2, 1}, {1, 2}, {-1, 2}, {-2, 1},
-    {-2, -1}, {-1, -2}, {1, -2}, {2, -1}
-};
+#define MAX 8
 
-bool validMove(size_t cord[2], size_t n, size_t m){
-    size_t x = cord[0];
-    size_t y = cord[1];
-    return ((x < n && y < m) && (x >= 0 && y >= 0));
+int dx[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+
+int n, m;
+int board[MAX][MAX];
+
+int is_valid(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m && board[x][y] == -1;
 }
 
-bool found = false;
+int count_onward(int x, int y) {
+    int cnt = 0;
+    for (int i = 0; i < 8; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (is_valid(nx, ny)) cnt++;
+    }
+    return cnt;
+}
 
-void knightPath(size_t r, size_t c, size_t n, size_t m, size_t board[n][m], size_t path[n*m][2], int k){
-    if(found) return;
-    if(k == n*m){
-        for(size_t i = 0; i < n*m; i++){
-            printf("%zu %zu\n", path[i][0], path[i][1]);
+int knights_tour(int x, int y, int move_num, int sx, int sy) {
+    board[x][y] = move_num;
+    if (move_num == n * m - 1) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i], ny = y + dy[i];
+            if (nx == sx && ny == sy) return 1;
         }
-        found = true;
-        return;
+        board[x][y] = -1;
+        return 0;
     }
-    size_t moves[8][2];
-    for (size_t i = 0; i < 8; i++) {
-        moves[i][0] = movements[i][0] + r;
-        moves[i][1] = movements[i][1] + c;
+
+    int next[8], idx[8];
+    for (int i = 0; i < 8; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (is_valid(nx, ny)) {
+            next[i] = count_onward(nx, ny);
+            idx[i] = i;
+        } else {
+            next[i] = 99;
+            idx[i] = i;
+        }
     }
-    for(size_t i = 0; i < 8; i++){
-        if(validMove(moves[i], n, m)){
-            size_t x = moves[i][0];
-            size_t y = moves[i][1];
-            if(board[x][y] == 0){
-                board[x][y] = 1;
-                path[k][0] = x;
-                path[k][1] = y;
-                knightPath(x, y, n, m, board, path, k + 1);
-                board[x][y] = 0;
-                path[k][0] = 0;
-                path[k][1] = 0;
+    for (int i = 0; i < 8; i++)
+        for (int j = i + 1; j < 8; j++)
+            if (next[j] < next[i]) {
+                int t = next[i]; next[i] = next[j]; next[j] = t;
+                t = idx[i]; idx[i] = idx[j]; idx[j] = t;
             }
+
+    for (int k = 0; k < 8; k++) {
+        int i = idx[k];
+        int nx = x + dx[i], ny = y + dy[i];
+        if (is_valid(nx, ny)) {
+            if (knights_tour(nx, ny, move_num + 1, sx, sy)) return 1;
         }
     }
+    board[x][y] = -1;
+    return 0;
 }
 
-int main(){
-    size_t n, m;
-    scanf("%zu %zu", &n, &m);
-    size_t board[n][m];
-    size_t path[n*m][2];
-    memset(path, 0, sizeof(path));
-    memset(board, 0, sizeof(board));
-    board[0][0] = 1;
-    path[0][0] = 0;
-    path[0][1] = 0;
-    knightPath(0, 0, n, m, board, path, 1);
-    if(!found) printf("No solution found.\n");
+int main() {
+    int r, c;
+    scanf("%d %d %d %d", &n, &m, &r, &c);
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            board[i][j] = -1;
+
+    if (knights_tour(r, c, 0, r, c)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                printf("%d ", board[i][j] + 1);
+            printf("\n");
+        }
+    } else {
+        printf("-1\n");
+    }
     return 0;
 }
