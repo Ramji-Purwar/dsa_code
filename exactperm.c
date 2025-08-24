b7ed2d2bb341418e37f87643dd5f419a07ec20c7
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-08-24 09:27:18 +0000

    solved exactperm

--- exactperm.c
+++ exactperm.c
@@ -2,45 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXN 100005
+#define MAX_LAST 20
+long long fact[MAX_LAST+1];
 
-long long fact[MAXN];
-
-void precompute_fact(int n) {
+void precompute_fact() {
     fact[0] = 1;
-    for (int i = 1; i <= n; i++)
-        fact[i] = fact[i - 1] * i;
+    for (int i = 1; i <= MAX_LAST; i++)
+        fact[i] = fact[i-1] * i;
+}
+
+void kth_perm(int *arr, int len, long long k) {
+    int used[MAX_LAST] = {0};
+    for (int i = 0; i < len; i++) {
+        long long f = fact[len-i-1];
+        int idx = k / f;
+        k %= f;
+        int cnt = 0;
+        for (int j = 0; j < len; j++) {
+            if (!used[j]) {
+                if (cnt == idx) {
+                    printf("%d ", arr[j]);
+                    used[j] = 1;
+                    break;
+                }
+                cnt++;
+            }
+        }
+    }
 }
 
 int main() {
     int n;
     long long k;
     scanf("%d %lld", &n, &k);
-
-    precompute_fact(n);
-
-    int *used = (int *)calloc(n + 1, sizeof(int));
-    int *perm = (int *)malloc(n * sizeof(int));
-
-    k--;
-
-    for (int i = 0; i < n; i++) {
-        int cnt = 0;
-        for (int j = 1; j <= n; j++) {
-            if (used[j]) continue;
-            if (k < fact[n - i - 1]) {
-                perm[i] = j;
-                used[j] = 1;
-                break;
-            }
-            k -= fact[n - i - 1];
-        }
+    precompute_fact();
+    if (n <= MAX_LAST) {
+        int arr[MAX_LAST];
+        for (int i = 0; i < n; i++) arr[i] = i+1;
+        kth_perm(arr, n, k-1);
+        printf("\n");
+    } else {
+        for (int i = 1; i <= n-MAX_LAST; i++) printf("%d ", i);
+        int arr[MAX_LAST];
+        for (int i = 0; i < MAX_LAST; i++) arr[i] = n-MAX_LAST+1+i;
+        kth_perm(arr, MAX_LAST, k-1);
+        printf("\n");
     }
-
-    for (int i = 0; i < n; i++)
-        printf("%d%c", perm[i], i == n - 1 ? '\n' : ' ');
-
-    free(used);
-    free(perm);
     return 0;
 }
