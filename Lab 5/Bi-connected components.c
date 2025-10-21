74b0d3cedb590d0dc7949277aa635d482241047d
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-10-21 18:11:50 +0000

    bi-connected components in progress

--- Lab 5/Bi-connected components.c
+++ Lab 5/Bi-connected components.c
@@ -4,10 +4,16 @@
 #include <stdbool.h>
 #define REP(i, a, b) for(int i = a; i < b; i++)
 
+int timer = 0;
+
 int min(int a, int b){
     if(a < b) return a;
     return b;
 }
+
+typedef struct Edge{
+    int u, v;
+} edge;
 
 typedef struct Node{
     int val;
@@ -26,6 +32,7 @@ typedef struct Graph{
     node** aj;
     int* d;
     int* l;
+    bool* mark;
 } graph;
 
 graph* cgraph(int n){
@@ -34,9 +41,11 @@ graph* cgraph(int n){
     g-> aj = malloc(n * sizeof(node*));
     g-> d = malloc(n * sizeof(int));
     g-> l = malloc(n * sizeof(int));
+    g-> mark = malloc(n * sizeof(bool));
     REP(i, 0, n){
         g-> aj[i] = NULL;
         g-> d[i] = g-> l[i] = -1;
+        g-> mark[i] = false;
     }
     return g;
 }
@@ -51,59 +60,31 @@ void addEdge(graph* g, int u, int v){
     g-> aj[v] = new;
 }
 
-void dfs(graph* g, int v, int p){
-    node* ch = g-> aj[v];
-    while(ch != NULL){
-        int el = ch-> val;
-        if(g-> d[el] == -1){
-            g-> d[el] = g-> l[el] = g-> d[v] + 1;
-            dfs(g, el, v);
-        }
-        ch = ch-> next;
-    }
-    ch = g-> aj[v];
-    while(ch != NULL){
-        int el = ch-> val;
-        if(el != p) g->l[v] = min(g->l[v], g->l[el]);
-        ch = ch-> next;
-    }
-}
+int cnt = 0;
 
-void dfs2(graph* g, int v){
+void dfs(graph* g, int v, int p){
+    g-> d[v] = g-> l[v] = cnt;
     node* ch = g-> aj[v];
-    g-> d[v] = 1;
+    int degree = 0;
     while(ch != NULL){
         int el = ch-> val;
-        if(g-> d[el] == 0 && g-> l[el] == g-> l[v]){
-            dfs2(g, el);
+        if(el != p){
+            if(g-> d[el] == -1){
+                dfs(g, el, v);
+                g->l[v] = min(g->l[v], g->l[el]);
+                if(g-> l[el] >= g-> l[v] && p != -1) g-> mark[v] = true;
+                degree++;
+            }
+            else{
+                g-> l[v] = min(g->l[v], g->d[el]);
+            }
         }
         ch = ch-> next;
     }
+    if(p == -1 && degree > 1) g-> mark[v] = true;
 }
 
 int main(){
     int n, m;
     scanf("%d %d", &n, &m);
-    graph* g = cgraph(n);
-    REP(i, 0, m){
-        int u, v;
-        scanf("%d %d", &u, &v);
-        addEdge(g, u-1, v-1);
-    }
-
-    g->d[0] = g->l[0] = 0;
-    dfs(g, 0, -1);
-
-    REP(i, 0, n) g-> d[i] = 0;
-
-    int cnt = 1;
-    REP(i, 0, n){
-        if(g-> d[i] == 0){
-            dfs2(g, i);
-            cnt++;
-        }
-    }
-    
-    printf("%d\n", cnt);
-    return 0;
 }
