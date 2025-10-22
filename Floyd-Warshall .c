37c2eb73c95c1c9457728a621d004ac2eaeacb95
Ramji Purwar <ramji.purwar@iitgn.ac.in>
2025-10-22 08:58:43 +0000

    implemented floyd-marshall algorithm

---
+++ Floyd-Warshall .c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+#define REP(i, a, b) for(int i = a; i < b; i++)
+#define ll long long
+const int INF = INT_MAX;
+
+ll min_ll(ll a, ll b){
+    return a > b ? b : a;
+}
+
+int n;
+ll** dist;
+
+int main(){
+    scanf("%d", &n);
+    dist = malloc(n * sizeof(ll*));
+    REP(i, 0, n){
+        dist[i] = malloc(n * sizeof(ll));
+        REP(j, 0, n){
+            if(i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
+    }
+
+    REP(i, 0, n){
+        int u, v;
+        ll wt;
+        scanf("%d %d %lld", &u, &v, &wt);
+        dist[u][v] = wt;
+    }
+
+    REP(k, 0, n){
+        REP(i, 0, n){
+            REP(j, 0, n){
+                dist[i][j] = min_ll(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    return 0;
+}
